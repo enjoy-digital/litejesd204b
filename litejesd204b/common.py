8c3c12e34fc8648449115dab7321c23a4b3a6e43
Florent Kermarrec <florent@enjoy-digital.fr>
2016-08-16 17:21:26 +0200

    common: initialize small memo

--- litejesd204b/common.py
+++ litejesd204b/common.py
@@ -1,3 +1,24 @@
+# XXX memo
+
+# JESD204B parameters
+# L:    Number of lanes per converter device, 1-8
+# M:    Number of converters per device, 1-256
+# F:    Number of octets per frame, 1, 2, 4-256
+# S:    Number of transmitted samples per converter per frame, 1-32
+# N:    Number of conversion bits per converter, 1-32
+# N':   Number of transmitted bits per sample, 1-32
+# K:    Number of frames per multiframe, 17/F ≤ K ≤ 32 ; 1-32
+# CS:   Number of control bits per conversion sample, 0-3
+# CF:   Number of control words per frame clock period per link, 0-32
+# HD:   High Density user data format,0 or 1
+# LMFC: Local multiframe clock, (F × K /4) link clock counts
+
+# JESD204B parameters relationship
+# JESD204B word size = N’
+# F = (M x S x N')/(8 x L)
+
+# XXX memo
+
 # Transport layer
 
 # Data link layer
