330e96e1e9216da6c1f2bc91e7521865aebcd382
Florent Kermarrec <florent@enjoy-digital.fr>
2016-08-17 11:52:26 +0200

    move scrambling to scrambling.py (scrambling is inserted between transport and link layer)

--- litejesd204b/core/link.py
+++ litejesd204b/core/link.py
@@ -1,13 +1,5 @@
 from litejesd204b.common import *
 
-
-class LiteJESD204BScrambler(Module):
-    """JESD204 Scrambler
-    """
-    def __init__(self):
-        # TODO (optional), polynom = 1 + X^14 + X^15
-
-
 class LiteJESD204BCharacterRemplacement(Module):
     """JESD204 Character Remplacement
     """
