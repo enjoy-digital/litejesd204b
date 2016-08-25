5c1c200ff05b1c2dda814c8b55efa5c516c5a202
Florent Kermarrec <florent@enjoy-digital.fr>
2016-08-25 13:52:09 +0200

    core: move code from scrambling.py to link.py

--- litejesd204b/core/link.py
+++ litejesd204b/core/link.py
@@ -1,6 +1,20 @@
 from litejesd204b.common import *
 
 
+class LiteJESD204BScrambler(Module):
+    """JESD204 Scrambler
+    """
+    def __init__(self):
+        # TODO, polynom = 1 + X^14 + X^15
+
+
+class LiteJESD204BDescrambler(Module):
+    """JESD204 Descrambler
+    """
+    def __init__(self):
+        # TODO, polynom = 1 + X^14 + X^15
+
+
 class LiteJESD204BCharacterRemplacement(Module):
     """JESD204 Character Remplacement
     """
