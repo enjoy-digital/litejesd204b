ed3684cfaab61f877ce8cc16d88f71769b7a5fcd
Florent Kermarrec <florent@enjoy-digital.fr>
2016-08-30 15:55:58 +0200

    test/model: remove verilog model and testbench (migen code & test now identical)

--- test/model/jesd204_scrambler.py
+++
@@ -1,8 +0,0 @@
-from litejesd204b.core.link import LiteJESD204BScrambler
-from litex.gen.fhdl import verilog
-
-scrambler = LiteJESD204BScrambler(32)
-ios = {scrambler.enable,
-       scrambler.data_in,
-       scrambler.data_out}
-print(verilog.convert(scrambler, ios))
