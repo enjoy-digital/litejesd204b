dbe5791b1bcad986cf713fa33cab5761798dd938
Florent Kermarrec <florent@enjoy-digital.fr>
2016-09-14 15:56:04 +0200

    test/test_transport: cleanup

--- test/test_transport.py
+++ test/test_transport.py
@@ -1,4 +1,5 @@
 import unittest
+from math import ceil
 
 from litex.gen import *
 from litex.gen.fhdl import verilog
@@ -9,23 +10,26 @@ from test.model.transport import samples_to_lanes
 
 
 class TestTransport(unittest.TestCase):
-    def test_transport_tx(self):
-        nlanes = 4
-        nconverters = 4
-
+    def test_transport_tx(self, nlanes=4, nconverters=4, converter_data_width=64):
         transport_settings = JESD204BTransportSettings(f=2, s=1, k=16, cs=1)
         physical_settings = JESD204BPhysicalSettings(l=nlanes, m=nconverters, n=16, np=16, sc=1*1e9)
 
-        transport = TransportTX(transport_settings, physical_settings, 64)
+        transport = TransportTX(transport_settings, physical_settings, converter_data_width)
 
         input_samples = [[j+i*256 for j in range(16)] for i in range(nconverters)]
-        lanes = samples_to_lanes(samples_per_frame=1,
-                                 nlanes=nlanes,
-                                 nconverters=nconverters,
-                                 nbits=16,
-                                 samples=input_samples)
+        reference_lanes = samples_to_lanes(samples_per_frame=1,
+                                           nlanes=nlanes,
+                                           nconverters=nconverters,
+                                           nbits=16,
+                                           samples=input_samples)
 
         dut_lanes = [[] for i in range(4)]
+
+
+        samples_per_frame = transport_settings.s        
+        nibbles_per_word = ceil(physical_settings.np//4)
+        octets_per_lane = samples_per_frame*nibbles_per_word//2
+        lane_data_width = len(transport.source.data0)
 
         def generator(dut):
             yield dut.sink.valid.eq(1)
@@ -34,19 +38,16 @@ class TestTransport(unittest.TestCase):
                 if i < 4:
                     for c in range(nconverters):
                         converter_data = getattr(dut.sink, "data"+str(c))
-                        yield converter_data.eq((input_samples[c][4*i]   << 0)  |
-                                                (input_samples[c][4*i+1] << 16) |
-                                                (input_samples[c][4*i+2] << 32) |
-                                                (input_samples[c][4*i+3] << 48))
+                        for j in range(nconverters):
+                            yield converter_data[16*j:16*(j+1)].eq(input_samples[c][4*i+j])
                 if i > 0:
                     for l in range(nlanes):
                         lane_data = (yield getattr(dut.source, "data"+str(l)))
-                        for f in range(4):
-                            frame = [(lane_data >> f*16)     & 0xff, 
-                                     (lane_data >> (f*16+8)) & 0xff]
-                            dut_lanes[l].append(frame)         
+                        for f in range(lane_data_width//(octets_per_lane*8)):
+                            frame = [(lane_data >> f*16+8*i) & 0xff for i in range(octets_per_lane)]
+                            dut_lanes[l].append(frame)
                 yield
 
         run_simulation(transport, generator(transport))
 
-        self.assertEqual(lanes, dut_lanes)
+        self.assertEqual(reference_lanes, dut_lanes)
