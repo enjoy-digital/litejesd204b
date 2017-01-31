d74b2424f5ac1c28e2ebc6efdd234ae16a37deb2
Florent Kermarrec <florent@enjoy-digital.fr>
2017-01-31 18:31:51 +0100

    phy: add transceiver parameter

--- litejesd204b/phy/__init__.py
+++ litejesd204b/phy/__init__.py
@@ -2,23 +2,27 @@ from litex.gen import *
 
 from litejesd204b.common import *
 from litejesd204b.phy.gtx import GTXTransmitter
+from litejesd204b.phy.gth import GTHTransmitter
 
 
 class LiteJESD204BPhyTX(Module):
-    def __init__(self, pll, tx_pads, sys_clk_freq):
+    def __init__(self, pll, tx_pads, sys_clk_freq, transceiver="gtx"):
         self.data = Signal(32)
         self.ctrl = Signal(32//8)
 
         # # #
 
-        # transceiver
-        self.submodules.gtx = GTXTransmitter(
-                pll=pll,
-                tx_pads=tx_pads,
-                sys_clk_freq=sys_clk_freq)
-
+        transmitters = {
+            "gtx": GTXTransmitter,
+            "gth": GTHTransmitter
+        }
+        self.submodules.transmitter = transmitters[transceiver](
+            pll=pll,
+            tx_pads=tx_pads,
+            sys_clk_freq=sys_clk_freq
+        )
         for i in range(32//8):
             self.comb += [
-                self.gtx.encoder.d[i].eq(self.data[8*i:8*(i+1)]),
-                self.gtx.encoder.k[i].eq(self.ctrl[i])
+                self.transmitter.encoder.d[i].eq(self.data[8*i:8*(i+1)]),
+                self.transmitter.encoder.k[i].eq(self.ctrl[i])
             ]
