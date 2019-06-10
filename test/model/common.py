5c1638c786d98ec9597b42e5845f303c49e89797
Florent Kermarrec <florent@enjoy-digital.fr>
2019-06-10 19:10:09 +0200

    test/model/common: cleanup seed_to_data

--- test/model/common.py
+++ test/model/common.py
@@ -6,7 +6,4 @@ def swap_bytes(data, n):
     return int.from_bytes(data.to_bytes(n, byteorder="little"), byteorder="big")
 
 def seed_to_data(seed, random=True):
-    if random:
-        return (seed * 0x31415979 + 1) & 0xffffffff
-    else:
-        return seed
+	return (seed * 0x31415979 + 1) & 0xffffffff if random else seed
