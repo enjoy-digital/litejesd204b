0dcd82f4b283d5bcfec353aeb1bb24819e4c4145
Florent Kermarrec <florent@enjoy-digital.fr>
2019-06-10 11:46:24 +0200

    test/test_transport: add transport_loopback_test to test LiteJESD204BTransportRX

--- test/test_transport.py
+++ test/test_transport.py
@@ -4,9 +4,9 @@ from math import ceil
 from migen import *
 
 from litejesd204b.common import *
-from litejesd204b.transport import LiteJESD204BTransportTX
+from litejesd204b.transport import LiteJESD204BTransportTX, LiteJESD204BTransportRX
 
-from test.model.transport import samples_to_lanes
+from test.model.transport import samples_to_lanes, lanes_to_samples
 
 
 class TestTransport(unittest.TestCase):
@@ -50,8 +50,47 @@ class TestTransport(unittest.TestCase):
         run_simulation(transport, generator(transport))
         return reference_lanes, output_lanes
 
-
     def test_transport_tx(self):
         for nlanes in [1, 2, 4, 8]:
             reference, output = self.transport_tx_test(nlanes, 4, 64)
+            self.assertEqual(reference, output)
+
+    def transport_loopback_test(self, nlanes, nconverters, converter_data_width):
+        ps = JESD204BPhysicalSettings(l=nlanes, m=nconverters, n=16, np=16)
+        ts = JESD204BTransportSettings(f=2, s=1, k=16, cs=1)
+        jesd_settings = JESD204BSettings(ps, ts, did=0x5a, bid=0x5)
+
+        class DUT(Module):
+            def __init__(self):
+                transport_tx = LiteJESD204BTransportTX(jesd_settings, converter_data_width)
+                transport_rx = LiteJESD204BTransportRX(jesd_settings, converter_data_width)
+                self.comb += transport_rx.sink.eq(transport_tx.source)
+                self.submodules += transport_tx, transport_rx
+                self.sink, self.source = transport_tx.sink, transport_rx.source
+
+        dut = DUT()
+        input_samples  = [[j+i*256 for j in range(16)] for i in range(nconverters)]
+        output_samples = [[] for i in range(nconverters)]
+
+        def generator(dut):
+            for i in range(5):
+                if i < 4:
+                    for c in range(nconverters):
+                        converter_data = getattr(dut.sink, "converter"+str(c))
+                        for j in range(nconverters):
+                            yield converter_data[16*j:16*(j+1)].eq(input_samples[c][4*i+j])
+                if i > 0:
+                    for c in range(nconverters):
+                        converter_data = (yield getattr(dut.source, "converter"+str(c)))
+                        for j in range(nconverters):
+                            sample = (converter_data >> 16*j) & 0xffff
+                            output_samples[c].append(sample)
+                yield
+
+        run_simulation(dut, generator(dut))
+        return input_samples, output_samples
+
+    def test_transport_loopback(self):
+        for nlanes in [1, 2, 4, 8]:
+            reference, output = self.transport_loopback_test(nlanes, 4, 64)
             self.assertEqual(reference, output)
