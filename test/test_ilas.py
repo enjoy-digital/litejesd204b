1c58cb87522b5cf6f5f0181817380ae05f80e805
Florent Kermarrec <florent@enjoy-digital.fr>
2019-06-11 13:19:10 +0200

    test/test_ilas: add ILASChecker test and add ctrls tests on ILASGenerator test

--- test/test_ilas.py
+++ test/test_ilas.py
@@ -5,10 +5,11 @@ from migen import *
 from test.model.common import swap_bytes
 
 from litejesd204b.common import *
-from litejesd204b.link import ILASGenerator
+from litejesd204b.link import ILASGenerator, ILASChecker
 
-# ilas reference sequence from a validated core
-ilas_reference = [
+# ILAS reference sequence (from a validated core)
+def ilas_datas_reference():
+    return [
     0x0302011c, 0x07060504, 0x0b0a0908, 0x0f0e0d0c,
     0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c,
     0x23222120, 0x27262524, 0x2b2a2928, 0x2f2e2d2c,
@@ -29,23 +30,78 @@ ilas_reference = [
     0xe3e2e1e0, 0xe7e6e5e4, 0xebeae9e8, 0xefeeedec,
     0xf3f2f1f0, 0xf7f6f5f4, 0xfbfaf9f8 , 0x7cfefdfc]
 
+def ilas_ctrls_reference():
+    return [
+    0x1, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x8,
+
+    0x3, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x8,
+
+    0x1, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x8,
+
+    0x1, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x0,
+    0x0, 0x0, 0x0, 0x8]
+
+
 class TestILAS(unittest.TestCase):
-    def test_ilas(self):
+    def test_ilas_generator(self):
         ps = JESD204BPhysicalSettings(l=4, m=4, n=14, np=16)
         ts = JESD204BTransportSettings(f=2, s=1, k=32, cs=2)
         jesd_settings = JESD204BSettings(ps, ts, did=0x55, bid=0xa)
 
-        ilas = ILASGenerator(32, 2, 32, jesd_settings.get_configuration_data())
+        dut = ILASGenerator(32, 2, 32, jesd_settings.get_configuration_data())
 
-        ilas_output = []
+        ilas_datas_output = []
+        ilas_ctrls_output = []
 
-        def generator(dut):
+        def checker(dut):
             yield dut.reset.eq(1)
             yield
             yield dut.reset.eq(0)
             while (yield dut.source.last) == 0:
                 yield
-                ilas_output.append((yield dut.source.data))
+                ilas_datas_output.append((yield dut.source.data))
+                ilas_ctrls_output.append((yield dut.source.ctrl))
 
-        run_simulation(ilas, generator(ilas))
-        self.assertEqual(ilas_reference, ilas_output)
+        run_simulation(dut, checker(dut))
+        self.assertEqual(ilas_datas_reference(), ilas_datas_output)
+        self.assertEqual(ilas_ctrls_reference(), ilas_ctrls_output)
+
+    def test_ilas_checker(self):
+        ps = JESD204BPhysicalSettings(l=4, m=4, n=14, np=16)
+        ts = JESD204BTransportSettings(f=2, s=1, k=32, cs=2)
+        jesd_settings = JESD204BSettings(ps, ts, did=0x55, bid=0xa)
+
+        dut = ILASChecker(32, 2, 32, jesd_settings.get_configuration_data())
+        dut._errors = 0
+
+        ilas_output = []
+
+        _ilas_datas_reference = ilas_datas_reference()
+        _ilas_ctrls_reference = ilas_ctrls_reference()
+
+        _ilas_datas_reference[0] ^= 1 # flip one bit on first data
+        _ilas_ctrls_reference[1] ^= 1 # flip one bit on second ctrl
+
+        def generator(dut):
+            yield dut.reset.eq(1)
+            yield
+            yield dut.reset.eq(0)
+            for data, ctrl in zip(_ilas_datas_reference, _ilas_ctrls_reference):
+                yield dut.sink.data.eq(data)
+                yield dut.sink.ctrl.eq(ctrl)
+                yield
+            dut._errors = (yield dut.errors)
+
+        run_simulation(dut, generator(dut))
+        self.assertEqual(dut._errors, 2)
