1e88fa8aa0b1165350ed6c68735cecd6e93da241
Florent Kermarrec <florent@enjoy-digital.fr>
2020-08-23 16:23:19 +0200

    add SPDX License identifier to header and specify file is part of LiteJESD204B.

--- litejesd204b/core.py
+++ litejesd204b/core.py
@@ -1,6 +1,9 @@
-# This file is Copyright (c) 2016-2019 Florent Kermarrec <florent@enjoy-digital.fr>
-# This file is Copyright (c) 2016 Robert Jordens <jordens@gmail.com>
-# License: BSD
+#
+# This file is part of LiteJESD204B
+#
+# Copyright (c) 2016-2019 Florent Kermarrec <florent@enjoy-digital.fr>
+# Copyright (c) 2016 Robert Jordens <jordens@gmail.com>
+# SPDX-License-Identifier: BSD-2-Clause
 
 from functools import reduce
 from operator import and_
