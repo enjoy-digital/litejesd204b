70525dae0b39bad003b1a92f71dd4768bf0acdc5
Florent Kermarrec <florent@enjoy-digital.fr>
2023-12-19 09:13:49 +0100

    setup.py: Improve indentation.

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ from setuptools import find_packages
 
 
 setup(
-    name="litejesd204b",
-    description="Small footprint and configurable JESD204B core",
-    author="Florent Kermarrec",
-    author_email="florent@enjoy-digital.fr",
-    url="http://enjoy-digital.fr",
-    download_url="https://github.com/enjoy-digital/litejesd204b",
-    test_suite="test",
-    license="BSD",
-    python_requires="~=3.6",
-    packages=find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
-    include_package_data=True,
+    name                 = "litejesd204b",
+    description          = "Small footprint and configurable JESD204B core",
+    author               = "Florent Kermarrec",
+    author_email         = "florent@enjoy-digital.fr",
+    url                  = "http://enjoy-digital.fr",
+    download_url         = "https://github.com/enjoy-digital/litejesd204b",
+    test_suite           = "test",
+    license              = "BSD",
+    python_requires      = "~=3.6",
+    packages             = find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
+    include_package_data = True,
 )
